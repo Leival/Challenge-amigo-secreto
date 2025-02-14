ea6f7becc95b1b14cc00223a9875ee1f0bdfb1ac
Angie Leiva <leiva00221@gmail.com>
2025-02-13 23:26:01 -0500

    uptade app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresParticipantes = [];
+console.log(nombresParticipantes);
+function añadirNombre(){
+    let nombres = document.getElementById('amigo').value;
+    console.log (nombres);
+   
+    if (nombres == 0) {
+    alert('Por favor, inserte un nombre.');
+   }
+   else{
+    let nuevo = nombresParticipantes.push (nombres);
+   limpiarCaja();
+}
+   return;
+}
+
+function limpiarCaja(){
+    let valorCaja = document.getElementById('amigo');
+    valorCaja.value = '';
+}
+function listaNombres(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "${nombres}";
+
+
+}
