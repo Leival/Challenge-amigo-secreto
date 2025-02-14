54e982ac80eb94d63a276ec1d22cf36eb7a69f33
Angie Leiva <leiva00221@gmail.com>
2025-02-14 18:14:34 -0500

    uptade app.js

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 let nombresParticipantes = [];
+let numeroIndice = generarIndice();
+console.log(numeroIndice);
 console.log(nombresParticipantes);
-function añadirNombre(){
-    let nombres = document.getElementById('amigo').value;
-    console.log (nombres);
-   
-    if (nombres == 0) {
-    alert('Por favor, inserte un nombre.');
-   }
-   else{
-    let nuevo = nombresParticipantes.push (nombres);
-   limpiarCaja();
-}
-   return;
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos"); //Obtiene una referencia al elemento HTML con el ID "listaAmigos", donde se mostrarán los nombres de los participantes.
+    lista.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
+    
+    for (let nombre of nombresParticipantes) { //  Itera sobre el arreglo nombresParticipantes, donde cada elemento representa un nombre.
+        // Para cada nombre en el arreglo:
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    }
 }
 
-function limpiarCaja(){
+function añadirNombre() {
+    let nombres = document.getElementById('amigo').value;
+    console.log(nombres);
+   
+    if (nombres === "") {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        nombresParticipantes.push(nombres);
+        limpiarCaja();
+        actualizarLista(); // Actualizar la lista después de añadir un nombre
+    }
+}
+
+function limpiarCaja() {
     let valorCaja = document.getElementById('amigo');
     valorCaja.value = '';
 }
-function listaNombres(){
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = "${nombres}";
+function sortearAmigo() {
+    let nombres = document.getElementById('amigo').value;
+    console.log(nombres);
+   
+    if (nombres === "") {
+        alert('Por favor, inserte un nombre.');
+    } else {
 
-
+    
+    }
 }
+
+function generarIndice(){
+    return Math.floor(Math.random()*10)+0;
+}
+
